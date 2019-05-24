e6e521ecbbe9741f0fb9c0357741bf56e462eacf
Yury Samarychau <qw4n7y@gmail.com>
2019-05-24 15:43:41 +0300

    keeping build in repo, setting "." as a base url

---
+++ gui/build/precache-manifest.ad651cbd9adcc9c73858c4832313c976.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5b774d6a4105acd743ce3cac85fc3a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "915fddcefa16322b6048",
+    "url": "./static/js/2.e5303367.chunk.js"
+  },
+  {
+    "revision": "fceb4f94e9d4bee4f3d9",
+    "url": "./static/js/main.9491c635.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
