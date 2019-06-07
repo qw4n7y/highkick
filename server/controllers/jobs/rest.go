bef2f7cc08f8659b64a5895551bc23503c6c8346
qw4n7y <qw4n7y@Users-MacBook-Pro.local>
2019-06-07 19:30:06 +0300

    new theme, job destroying, etc

---
+++ server/controllers/jobs/rest.go
@@ -0,0 +1,37 @@
+package jobs
+
+import (
+	"net/http"
+
+	"github.com/qw4n7y/highkick/repository"
+
+	"github.com/gin-gonic/gin"
+)
+
+type destroyURIParams struct {
+	JobID int32 `uri:"job_id" binding:"required"`
+}
+
+// Destroy .
+func Destroy(c *gin.Context) {
+	var params destroyURIParams
+	if err := c.ShouldBindUri(&params); err != nil {
+		c.JSON(422, gin.H{"msg": err})
+		return
+	}
+
+	job := repository.GetJobByID(params.JobID)
+	jobs := repository.GetJobTree(job)
+	for _, j := range jobs {
+		if err := repository.DestroyJobLogsFor(j); err != nil {
+			c.JSON(422, gin.H{"msg": err})
+			return
+		}
+		if err := repository.DestroyJob(j); err != nil {
+			c.JSON(422, gin.H{"msg": err})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, struct{}{})
+}
