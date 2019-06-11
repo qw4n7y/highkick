6d4b34864c516618bb5854da71b1481700354599
qw4n7y <qw4n7y@Users-MacBook-Pro.local>
2019-06-11 11:53:26 +0300

    preparing for production env

---
+++ gui/build/precache-manifest.d5fe25302f110d187c35d285e9ede881.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2434f8bff71b7934456bcbabd7c9e17c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a5519f514de04f3c2e98",
+    "url": "./static/js/2.a802f52b.chunk.js"
+  },
+  {
+    "revision": "208ef7716a3ffe485a4a",
+    "url": "./static/js/main.d67fbab8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
