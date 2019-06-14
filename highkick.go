35aa17affbe9178fc5eee8c3f11913ba1fcd3840
qw4n7y <qw4n7y@Users-MacBook-Pro.local>
2019-06-14 13:50:31 +0300

    providing sexy api

--- highkick.go
+++ highkick.go
@@ -1,7 +1,23 @@
 package highkick
 
+import (
+	"github.com/qw4n7y/highkick/database"
+	"github.com/qw4n7y/highkick/jobs"
+	"github.com/qw4n7y/highkick/models"
+	"github.com/qw4n7y/highkick/server"
+)
+
 // Data source names
 const (
 	DevDataSourceName  = "root:root@tcp(127.0.0.1:3307)/highkick?clientFoundRows=true&charset=utf8mb4&parseTime=true&multiStatements=true"
 	TestDataSourceName = "root:root@tcp(127.0.0.1:3307)/highkick_test?clientFoundRows=true&charset=utf8mb4&parseTime=true&multiStatements=true"
 )
+
+// Provide liblary API
+var Setup = database.Setup
+var NewJob = models.BuildJob
+var Run = jobs.ManagerSingleton.RunJob
+var Register = jobs.ManagerSingleton.RegisterWorker
+var RegisterGuiBackend = server.Register
+
+type Input = models.JSONDictionary
