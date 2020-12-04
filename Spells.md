eb484d93b9cb7cde038f1a2a2f32cc35d0f9b37f
qw4n7y <qw4n7y@Users-MacBook-Pro.local>
2020-12-04 12:38:42 +0200

    azaza

---
+++ Spells.md
@@ -0,0 +1,2 @@
+git tag -a v0.2.1 -m "v0.2.1"
+git push --follow-tags origin
---
+++ client/build/precache-manifest.614143d16b6de207aa1e0bda9c9c068e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d505c0f84cf7d7639d0d693d8a7a1b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b3a5f9e9d765cd27f47",
+    "url": "./static/js/2.49481204.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "./static/js/2.49481204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00b040a136fe7d713c2",
+    "url": "./static/js/main.b65d5a8f.chunk.js"
+  },
+  {
+    "revision": "b82605588eeff63d3e9f",
+    "url": "./static/js/runtime-main.8235ad62.js"
+  }
+]);
